c1924d1142a708bfc7f69da62734d6feb0d94234
Iv00vI <goran.ivo11@gmail.com>
2025-09-11 17:53:33 +0200

    Enabled Pagenation

--- meter/src/main/java/com/ivoovi/meter/service/SubscriptionService.java
+++ meter/src/main/java/com/ivoovi/meter/service/SubscriptionService.java
@@ -4,7 +4,6 @@ import com.ivoovi.meter.dto.SubscriptionDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface SubscriptionService {
 
--- meter/src/main/java/com/ivoovi/meter/service/SubscriptionServiceImpl.java
+++ meter/src/main/java/com/ivoovi/meter/service/SubscriptionServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class SubscriptionServiceImpl implements SubscriptionService{
